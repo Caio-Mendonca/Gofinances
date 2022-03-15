69e1fa4c52cf758ddfa3cefdf39cf33c03fff75c
Caio <caioeduardojm4@gmail.com>
2022-03-15 10:33:30 -0300

    rewrite

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default{
+    colors:{
+        primary:'#5336D3',
+        secundary:'#FF872C',
+        secundary_light:'rgba(255, 135, 44, 0.3)',
+        sucess: '#12A454',
+        sucess_light: 'rgba(18, 164, 84, 0.5)',
+        attention: '#E83F5B',
+        attention_light:'rgba(232,63,91,0.5',
+        shape: '#FFFFF',
+        title: '#363F5F',
+        text: '#969cb2',
+        background:'#f0f2f5'
+    }
+}
